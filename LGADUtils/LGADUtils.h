245440f0d3f7629ecbf08b54882e897e06aeae67
Vagelis Gkougkousis <vagelis.gkougkousis@cern.ch>
2020-12-05 10:58:54 +0000

    Replace LGADUtils.h

--- LGADUtils/LGADUtils.h
+++ LGADUtils/LGADUtils.h
@@ -11,7 +11,6 @@
 #define __LGADUtils__
 
 #include "LGADUtils/LGADRun.h"
-#include "LGADUtils/LGADBase.h"
 #include "TChain.h"
 
 class LGADUtils : public LGADRun {
@@ -25,22 +24,27 @@ public:
    void SetFEi4Eff(bool FEi4Eff);
    bool SetSRate(double rate, unsigned int ch);
    bool SetNPoints(unsigned int points, unsigned int ch);
+   void Initialize();
    void SetFitMethode(std::string method);
    void SetInstrument(unsigned int instr);
-   void SetStartStopEvnt(unsigned int Evnt1 = 0, unsigned int Evnt2 = 0);
+   void SetStartStopEvnt(int Evnt1 = 0, int Evnt2 = 0);
    bool ConvertData();
+   void SetTreeName(std::string treename);
    void SetInDataNames(TString DataDir = "", TString DataName = "", TString ext = "");
    void SetOutDataNames(TString DataDir = "", TString DataName = "");
    void SetTrackInDataNames(TString DataDir = "", TString DataName = "");
    void SetVerbose(int verbose);
    void SetWaveShape(bool shape = false);
+   void SetDoTrnsCorr(bool TrnsCorr = false);
+   void SetTransFileName(TString filename = "");
    bool Analyse(Long64_t nprocess = 0, std::string tree = "");
+   std::string GetTreeName() { return LGADBase::GetTreeName(); };
 
 private:
 
-   LGADBase* m_Base;
    LGADRun *m_Run;
    TChain *m_chain;
+   LGADBase *m_Base;
 
 };
 
