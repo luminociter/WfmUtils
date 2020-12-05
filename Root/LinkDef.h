9076b4c05cb59e06be1bd6dd3883404d1863c519
Vagelis Gkougkousis <vagelis.gkougkousis@cern.ch>
2020-12-05 11:09:07 +0000

    Replace LinkDef.h

--- Root/LinkDef.h
+++ Root/LinkDef.h
@@ -2,13 +2,13 @@
 
 #pragma link off all globals;
 #pragma link off all classes;
-#pragma link off all functions;   
+#pragma link off all functions;
 #pragma link C++ nestedtypedef;
 #pragma link off all typedef;
 #pragma link C++ nestedclass;
 #pragma link C++ class LGADBase;
-#pragma link C++ class LGADSel;
 #pragma link C++ class WaveForm;
+#pragma link C++ class LGADSel;
 #pragma link C++ class DUTChannel;
 #pragma link C++ class LGADRun;
 #pragma link C++ class LGADUtils;
